c23bff14667438a73625483bbcc6b0f5bdf9870b
Laura Ebel <laura.ebel7@gmail.com>
2023-09-25 18:13:02 -0400

    Created first table

--- cafe.sql
+++ cafe.sql
@@ -1,1 +1,8 @@
-SELECT NOW();
+CREATE TABLE cafe.inventory (
+id INT NOT NULL,
+item VARCHAR (255),
+category VARCHAR(255),
+unit_cost DECIMAL (18,2),
+units_available INT NOT NULL,
+units_ordered INT);
+
